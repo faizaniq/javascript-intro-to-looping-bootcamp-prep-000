6b2e7b118c54c0b3d022d26926f36b5a06d21579
Faizan Iqbal <f.iqbal.w@gmail.com>
2018-11-05 02:38:15 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -3,7 +3,7 @@ function forLoop(array) {
     if (i === 1) {
       array.push("I am 1 strange loop.")
     } else {
-      array.push("I am ${i} strange loops.")
+      array.push("I am " i " strange loops.")
     }
   }
   return array
@@ -14,4 +14,8 @@ function whileLoop(number) {
     console.log(--number)
   }
   return "done"
+}
+
+function doWhileLoop(array) {
+
 }
